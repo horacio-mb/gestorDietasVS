26648044bcad1caffc771806d7c343374e0f5d32
Horacio Molina <horaciomolinab0@gmail.com>
2021-05-19 01:07:42 -0400

    wsDieta - hastaBuscar

--- gestorDietas/proyectoDieta/wsDieta.asmx.cs
+++ gestorDietas/proyectoDieta/wsDieta.asmx.cs
@@ -19,9 +19,8 @@ namespace proyectoDieta
     public class wsDieta : System.Web.Services.WebService
     {
 
-
         [WebMethod]
-        public DataSet guardarDieta(string nombre, string fechaInicio, string fechaFinal, int idCliente,int idUsuario)
+        public DataSet guardarDieta(string nombre, string fechaInicio, string fechaFinal, string idCliente,string idUsuario)
         {
             clsConexion con = new clsConexion();
             string s;
@@ -32,14 +31,36 @@ namespace proyectoDieta
         }
 
         [WebMethod]
-        public DataSet buscarVentaPorFecha(string fecha1, string fecha2)
+        public DataSet modificarDieta(string idDieta,string nombre, string fechaInicio, string fechaFinal, string idCliente, string idUsuario)
         {
             clsConexion con = new clsConexion();
             string s;
-            s = "select id_venta, empresa, date_format(fecha, '%Y-%m-%d') as fecha from cliente, venta where venta.id_cliente=cliente.id_cliente and fecha between ('" + fecha1 + "') and ('" + fecha2 + "');";
+            s = "update dieta set nombre="+nombre+ ", fechaInicio="+ fechaInicio + ",  fechaFinal="+ fechaFinal + ", idCliente="+ idCliente + ", idUsuario="+ idUsuario + " where idDieta="+idDieta+" ;";
             DataSet ds = new DataSet();
             con.ejecutarSQL(s, "tc", ds);
             return ds;
         }
+
+        [WebMethod]
+        public DataSet eliminarDieta(string idDieta)
+        {
+            clsConexion con = new clsConexion();
+            string s;
+            s = "delete from dieta where idDieta = "+ idDieta + "; ";
+            DataSet ds = new DataSet();
+            con.ejecutarSQL(s, "tc", ds);
+            return ds;
+        }
+        //buscar dieta averiguar que parámetros 
+        public DataSet buscarDietaPorCliente(string buscar)
+        {
+            clsConexion con = new clsConexion();
+            string s;
+            s = "select d.idDieta, d.nombre, d.fechaInicio, d.fechaFinal , concat_ws(' ',c.nombre, c.apellido_paterno) as cliente from dieta d inner join cliente c on c.idCliente=d.idCliente where concat_ws(' ',c.nombre, c.apellido_paterno) like concat('%',"+ buscar + ",'%');";
+            DataSet ds = new DataSet();
+            con.ejecutarSQL(s, "tc", ds);
+            return ds;
+        }
+
     }
 }
